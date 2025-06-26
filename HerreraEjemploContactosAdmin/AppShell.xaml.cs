19a982c1f7208468df12c5944876fc9650d2f2be
laboratorio <laboratorio@UPOAULA10710>
2025-06-26 07:30:43 -0500

    Agregar archivos de proyecto.

---
+++ HerreraEjemploContactosAdmin/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HerreraEjemploContactosAdmin
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HerreraEjemploContactosAdmin/Models/Contacto.cs
@@ -0,0 +1,26 @@
+﻿
+using SQLite;
+
+namespace HerreraEjemploContactosAdmin.Models
+{
+    [Table("ContactoFinal")]
+    class Contacto
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(100), NotNull]
+        public string Nombre { get; set; }
+
+        [MaxLength(100), Unique]
+        public string Correo { get; set; }
+
+        [MaxLength(10), NotNull]
+        public string Telefono { get; set; }
+
+        public string Direccion { get; set; }
+
+        public string Empresa { get; set; }
+
+    }
+}
